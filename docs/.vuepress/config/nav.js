83ded62933062153a2fc5d4dbf70db4e49341ddb
pangkehan <pangkehan@buaa.edu.cn>
2024-09-16 03:23:17 +0800

    code

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -3,18 +3,31 @@ module.exports = [
     {text: 'Home', link: '/index.html'},
     {text: 'About', link: '/about/'},
     {text: 'C.V.', link: 'https://khancat1120.github.io/cv.pdf'},
-    {text: 'Surv.', link: '/knowledge/Survey/01-Textgraph'},
+    {
+        text: 'Surv.', 
+        items: [
+
+                {
+                    text: 'Surveys',
+                    link: '/knowledge/Survey/01-Textgraph'
+                },
+                {
+                    text: 'LLMs',
+                    link:'/technology/Survey/01-Textgraph'
+                },
+            ]
+    },
     {
         text: 'Tech.', 
         items: [
 
                 {
                     text: 'Codes',
-                    link:'/technology/code/01-GNN'
+                    link: '/technology/code/01-GNN'
                 },
                 {
                     text: 'Bugs',
-                    link:'/technology/bug/bug'
+                    link: '/technology/bug/bug'
                 },
             ]
     },
